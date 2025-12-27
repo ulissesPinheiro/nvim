d4312e42f257ebea5d3fcb3ec2b855e3b4d98977
Ulisses Pinheiro <ulisses.pinheiro@f4g.com.br>
2025-12-27 10:32:08 -0400

    feat(dbui): enable line numbers in DBUI tree view
    Added an autocommand to enable absolute line numbers when opening the
    DBUI
    tree. This improves navigation and usability within the database UI
    buffer.

--- lua/plugins/dadbod.lua
+++ lua/plugins/dadbod.lua
@@ -13,5 +13,13 @@ return {
 	init = function()
 		-- Your DBUI configuration
 		vim.g.db_ui_use_nerd_fonts = 1
+		-- Enable line numbers in DBUI tree
+		vim.api.nvim_create_autocmd("FileType", {
+			pattern = "dbui",
+			callback = function()
+				vim.wo.number = true
+				vim.wo.relativenumber = false
+			end,
+		})
 	end,
 }
